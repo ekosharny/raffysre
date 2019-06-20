c5fd0e9a623edf1d759a42b65234377732082426
elonakosharny <ekosharny@gmail.com>
2019-06-20 01:34:31 -0400

    modified tabs, added files, etc

---
+++ app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyATUOH0n4xFzqpdVZ-dcClwj7dJXqMVUiA",
+  authDomain: "raffysre.firebaseapp.com",
+  databaseURL: "https://raffysre.firebaseio.com",
+  projectId: "raffysre",
+  storageBucket: "",
+  messagingSenderId: "930139124274",
+  appId: "1:930139124274:web:50fe23c77e877391"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
