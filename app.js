6749a92308d812bd294518efefafd054c609ac91
elonakosharny <ekosharny@gmail.com>
2019-07-06 02:05:22 -0400

    updates

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyATUOH0n4xFzqpdVZ-dcClwj7dJXqMVUiA",
-  authDomain: "raffysre.firebaseapp.com",
-  databaseURL: "https://raffysre.firebaseio.com",
-  projectId: "raffysre",
-  storageBucket: "",
-  messagingSenderId: "930139124274",
-  appId: "1:930139124274:web:50fe23c77e877391"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const fridgeList = document.querySelector('#fridge-list');
+
+function renderFridges(doc){
+
+	let li = document.createElement('li');
+	let div = document.createElement('div');
+	let img = document.createElement('img');
+	let company = document.createElement('h5');
+	let model = document.createElement('p');
+
+	li.setAttribute('data-id', doc.id);
+	div.className = "card";
+	img.className="listing-image";
+	img.src = doc.data().img;
+	company.textContent = doc.data().company;
+	model.textContent = doc.data().model;
+
+	div.appendChild(img);
+	div.appendChild(company);
+	div.appendChild(model);
+
+	li.appendChild(div);
+
+	fridgeList.appendChild(li);
+}
+
+
+db.collection('refrigerators').get().then((snapshot) => {
+	snapshot.docs.forEach(doc => {
+		renderFridges(doc);
+	})
+})
